6adda09df0f8342bee0eaa6ac4f24ae6c2d5bbbd
kana-goto <tempesta2268@gmail.com>
2023-04-09 12:32:50 +0900

    予約詳細画面　CSS

--- resources/views/authenticated/calendar/admin/reserve_detail.blade.php
+++ resources/views/authenticated/calendar/admin/reserve_detail.blade.php
@@ -6,14 +6,14 @@
     <p><span>{{ $date }}日</span><span class="ml-3">{{ $part }}部</span></p>
     <div class="h-75 border">
       <table class="">
-        <tr class="text-center">
+        <tr class="text-center reserve">
           <th class="w-25">ID</th>
           <th class="w-25">名前</th>
           <th class="w-25">場所</th>
         </tr>
         @foreach($reservePersons as $reservePerson)
         @foreach($reservePerson-> users as $user)
-        <tr class="text-center">
+        <tr class="text-center reserve-user">
           <td class="w-25">{{$user->id}}</td>
           <td class="w-25">{{$user->over_name}}{{$user->under_name}}</td>
           <td class="w-25">リモート</td>
