0a5191bf951716afc1485e2d43970551aa647456
kana-goto <tempesta2268@gmail.com>
2023-04-17 23:28:07 +0900

    掲示板CSS修正

--- resources/views/authenticated/bulletinboard/posts.blade.php
+++ resources/views/authenticated/bulletinboard/posts.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="board_area w-100 border m-auto d-flex">
   <div class="post_view w-75 mt-5">
-    <p class="w-75 m-auto">投稿一覧</p>
+    <!-- <p class="w-75 m-auto">投稿一覧</p> -->
     @foreach($posts as $post)
     <div class="post_area border w-75 m-auto p-3">
       <p><span>{{ $post->user->over_name }}</span><span class="ml-3">{{ $post->user->under_name }}</span>さん</p>
       <p><a href="{{ route('post.detail', ['id' => $post->id]) }}">{{ $post->post_title }}</a></p>
 
-
-
-
-      <div class="post_bottom_area d-flex">
-        <div class="d-flex post_status">
+      <div class="post_bottom_area">
+        @foreach ($post-> subCategories as $sub_category)
+        <span class="btn btn-primary">{{ $sub_category->sub_category }}</span>
+        @endforeach
+        <div class="d-flex justify-content-end post_status">
           <div class="mr-5">
             <i class="fa fa-comment" post_id="{{ $post->id }}"></i><span class="post_comment_counts{{ $post->id }}">{{ $post_comment->commentCounts($post->id) }}</span>
           </div>
@@ -38,9 +38,12 @@
       </div>
       <input type="submit" name="like_posts" class="category_btn" value="いいねした投稿" form="postSearchRequest">
       <input type="submit" name="my_posts" class="category_btn" value="自分の投稿" form="postSearchRequest">
+      <p>カテゴリー検索</p>
       <ul>
         @foreach($categories as $category)
-        <li class="main_categories" category_id="{{ $category->id }}"><span>{{ $category->main_category }}<span></li>
+        <li class="main_categories" category_id="{{ $category->id }}">
+          <span>{{ $category->main_category }}<span>
+        </li>
         @foreach ($category -> subCategories as $sub_category)
         <input type="submit" name="category_word" class="category_btn" value="{{ $sub_category->sub_category }}" form="postSearchRequest">
         @endforeach
